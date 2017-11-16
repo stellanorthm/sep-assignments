027029a1c5c65116894cb3cc4622cc91088c05df
Diego Aguilar <diego.a.mazzero@gmail.com>
2017-11-16 07:51:24 -0800

    Checkpoint 03: Linked lists

---
+++ 01-data-structures/03-linked-lists/benchmark.rb
@@ -0,0 +1,40 @@
+require 'benchmark'
+require_relative 'node'
+require_relative 'linked_list'
+
+array = []
+new_list = LinkedList.new
+
+p "Node of 10,000"
+puts Benchmark.measure {
+  (0...10000).each do |x|
+    new_list.add_to_tail(Node.new(x))
+  end
+}
+
+p "Array access 500th index"
+puts Benchmark.measure {
+  array[4999]
+}
+
+p "Access 500th Node"
+puts Benchmark.measure {
+  node = new_list.head
+  (1..4999).each do |x|
+    node = node.next
+  end
+}
+
+p "Array delete 500th index"
+puts Benchmark.measure {
+  array.delete_at(4999)
+}
+
+p "Delete 500th Node"
+puts Benchmark.measure {
+  node = new_list.head
+  (1..4999).each do |x|
+    node = node.next
+  end
+  new_list.delete(node)
+}
--- 01-data-structures/03-linked-lists/linked_list.rb
+++ 01-data-structures/03-linked-lists/linked_list.rb
@@ -6,25 +6,45 @@ class LinkedList
 
   # This method creates a new `Node` using `data`, and inserts it at the end of the list.
   def add_to_tail(node)
+    if @head == nil
+      @head = node
+    elsif @tail
+      @tail.next = node
+    end
+    @tail = node
+    @tail.next = nil
   end
 
   # This method removes the last node in the lists and must keep the rest of the list intact.
   def remove_tail
+    @tail = @tail.prev
+    @tail.next = nil
   end
 
   # This method prints out a representation of the list.
   def print
+    puts node.data
+    node = node.next
   end
 
   # This method removes `node` from the list and must keep the rest of the list intact.
   def delete(node)
+      current_node = @head
+      until current_node.next == node
+        current_node = current_node.next
+      end
+      current_node.next = node.next
   end
 
   # This method adds `node` to the front of the list and must set the list's head to `node`.
   def add_to_front(node)
+    node.next = @head
+    @head = node
   end
 
   # This method removes and returns the first node in the Linked List and must set Linked List's head to the second node.
   def remove_front
+    @head = @head.next
+    @head.prev = nil
   end
 end
